f8ec9d2b653f430f10c429567d3dbb8ea6777c5b
Anderson Benjumea <anderson261014@gmail.com>
2022-08-23 01:56:10 -0500

    Update db.js

--- servidor/app/config/db.js
+++ servidor/app/config/db.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql');
 
 const conexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    port: '3306',
-    database:'youhao'
+    host: '151.106.98.0',
+    user: 'u159554062_youhaoapp',
+    password: 'Soporte2020*',
+    database:'u159554062_Youhaoapp'
 });
 
 conexion.connect((err) => {
